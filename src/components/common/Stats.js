1291487db7172df27eaed1b5225d48ac1ed91e2d
ilzheev <anton@lovedesign.ru>
2021-10-28 17:39:21 +0200

    metrics API ready

--- src/components/common/Stats.js
+++ src/components/common/Stats.js
@@ -39,48 +39,9 @@ const Stats = props => {
 
   const getStats = async () => {
     try {
-        const response = await RPC.request("metrics");
+        const response = await RPC.request("metrics", { metric: "tps", duration: "1h" });
 
-        setTPS(response.data.tps);
-        setAccounts(response.data.tps);
-        setTxs(response.data.tps);
-
-        if (response.data.prevtps && response.data.prevtps > 0) {
-            let change = Math.round(((response.data.tps/response.data.prevtps-1)*100) * 100) / 100;
-            setTPSChange(change);
-            if (change > 0) {
-                setTPSChangeClass("up");
-                setTPSChangeIcon(<ArrowUpOutlined />);
-            } else if (change < 0) {
-                setTPSChange(change*-1);
-                setTPSChangeClass("down");
-                setTPSChangeIcon(<ArrowDownOutlined />);
-            }
-        }
-        if (response.data.prevaccounts && response.data.prevaccounts > 0) {
-            let change = Math.round(((response.data.accounts/response.data.prevaccounts-1)*100) * 100) / 100;
-            setAccountsChange(change);
-            if (change > 0) {
-                setAccountsChangeClass("up");
-                setAccountsChangeIcon(<ArrowUpOutlined />);
-            } else if (change < 0) {
-                setAccountsChange(change*-1);
-                setAccountsChangeClass("down");
-                setAccountsChangeIcon(<ArrowDownOutlined />);
-            }
-        }
-        if (response.data.prevTxs && response.data.prevTxs > 0) {
-            let change = Math.round(((response.data.txs/response.data.prevTxs-1)*100) * 100) / 100;
-            setTxsChange(change);
-            if (change > 0) {
-                setTxsChangeClass("up");
-                setTxsChangeIcon(<ArrowUpOutlined />);
-            } else if (change < 0) {
-                setTxsChange(change*-1);
-                setTxsChangeClass("down");
-                setTxsChangeIcon(<ArrowDownOutlined />);
-            }
-        }
+        setTPS(response.data.value);
 
     }
     catch(error) {
@@ -113,99 +74,6 @@ const Stats = props => {
                     </div>
                 }
             </Card>
-          </Col>
-          <Col xs={0} sm={8} md={8} lg={6} xl={5}>
-            <Card>
-                <span>
-                    <IconContext.Provider value={{ className: 'react-icons' }}><RiAccountCircleLine /></IconContext.Provider>
-                    <br />
-                    Active Accounts <small>/ 24h</small>
-                </span>
-                {accounts === -1 ? (
-                    <Skeleton active paragraph={false} />
-                ) : 
-                    <div>
-                    <Title level={3} className="code">{accounts.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</Title>
-                    {displayChange === true ? (
-                    <Text className={"change change-"+accountsChangeClass}>{accountsChangeIcon}{accountsChange} %</Text>
-                    ) :
-                    null
-                    }
-                    </div>
-                }
-            </Card>
-          </Col>
-          <Col xs={0} sm={8} md={8} lg={6} xl={5}>
-            <Card>
-                <span>
-                    <IconContext.Provider value={{ className: 'react-icons' }}><RiExchangeLine /></IconContext.Provider>
-                    <br />
-                    Transactions <small>/ 24h</small>
-                </span>
-                {txs === -1 ? (
-                    <Skeleton active paragraph={false} />
-                ) : 
-                    <div>
-                    <Title level={3} className="code">{txs.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</Title>
-                    {displayChange === true ? (
-                    <Text className={"change change-"+txsChangeClass}>{txsChangeIcon}{txsChange} %</Text>
-                    ) :
-                    null
-                    }
-                    </div>
-                }
-            </Card>
-          </Col>
-        </Row>
-        <Row gutter={16}>
-          <Col xs={24} sm={0} md={0} lg={0} xl={0}>
-            <Carousel autoplay>
-                <Card>
-                    <span>
-                        <IconContext.Provider value={{ className: 'react-icons' }}><RiLineChartFill /></IconContext.Provider>
-                        <br />
-                        TPS <small>/ 24h</small>
-                    </span>
-                    {tps === -1 ? (
-                        <Skeleton active paragraph={false} />
-                    ) : 
-                        <div>
-                        <Title level={3} className="code">{tps.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</Title>
-                        <Text className={"change change-"+tpsChangeClass}>{tpsChangeIcon}{tpsChange} %</Text>
-                        </div>
-                    }
-                </Card>
-                <Card>
-                    <span>
-                        <IconContext.Provider value={{ className: 'react-icons' }}><RiAccountCircleLine /></IconContext.Provider>
-                        <br />
-                        Active Accounts <small>/ 24h</small>
-                    </span>
-                    {accounts === -1 ? (
-                        <Skeleton active paragraph={false} />
-                    ) : 
-                        <div>
-                        <Title level={3} className="code">{accounts.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</Title>
-                        <Text className={"change change-"+accountsChangeClass}>{accountsChangeIcon}{accountsChange} %</Text>
-                        </div>
-                    }
-                </Card>
-                <Card>
-                    <span>
-                        <IconContext.Provider value={{ className: 'react-icons' }}><RiExchangeLine /></IconContext.Provider>
-                        <br />
-                        Transactions <small>/ 24h</small>
-                    </span>
-                    {txs === -1 ? (
-                        <Skeleton active paragraph={false} />
-                    ) : 
-                        <div>
-                        <Title level={3} className="code">{txs.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</Title>
-                        <Text className={"change change-"+txsChangeClass}>{txsChangeIcon}{txsChange} %</Text>
-                        </div>
-                    }
-                </Card>
-            </Carousel>
           </Col>
         </Row>
       </div>
