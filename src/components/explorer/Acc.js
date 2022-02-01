51ba520d7ec05e5cb13438736d007d0830558d18
Forrest Gump <zigzag.way@gmail.com>
2022-02-01 14:10:07 +0100

    EX-46

--- src/components/explorer/Acc.js
+++ src/components/explorer/Acc.js
@@ -93,7 +93,7 @@ const Acc = ({ match }) => {
 
     return (
         <div>
-            <Title level={2} className="break-all">Chain</Title>
+            <Title level={2} className="break-all">Account</Title>
             <Title level={4} type="secondary" style={{ marginTop: "-10px" }} className="break-all" copyable>{accountURL}</Title>
                 {acc ? (
                     <Render data={acc} />
