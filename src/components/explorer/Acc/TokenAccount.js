31594649faadd7b204557dab6726d06580a20b9e
Forrest Gump <zigzag.way@gmail.com>
2022-02-08 17:56:58 +0100

    EX-49 Fix

--- src/components/explorer/Acc/TokenAccount.js
+++ src/components/explorer/Acc/TokenAccount.js
@@ -314,7 +314,7 @@ const TokenAccount = props => {
 
                         {tokenAccount.data.keyBook ? (
                             <Descriptions.Item label={<span><nobr><IconContext.Provider value={{ className: 'react-icons' }}><Tooltip overlayClassName="explorer-tooltip" title={tooltipDescs.keyBook}><RiQuestionLine /></Tooltip></IconContext.Provider>Key Book</nobr></span>}>
-                                <Link to={'/acc/' + tokenAccount.data.keyBook}><IconContext.Provider value={{ className: 'react-icons' }}><RiStackLine /></IconContext.Provider>{tokenAccount.data.keyBook}</Link>
+                                <Link to={'/acc/' + tokenAccount.data.keyBook.replace("acc://", "")}><IconContext.Provider value={{ className: 'react-icons' }}><RiStackLine /></IconContext.Provider>{tokenAccount.data.keyBook}</Link>
                             </Descriptions.Item>
                         ) :
                             null
