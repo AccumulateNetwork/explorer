f1779cd4e2787830718741161bd69ec47d859807
Ethan Reesor <ethan.reesor@gmail.com>
2024-05-24 14:22:37 -0500

    Fix rxjs

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,11 @@ import App from './App';
 import { Web3ReactProvider } from '@web3-react/core';
 import { Web3 } from 'web3';
 
+// Something depends on rxjs. I don't know what it is, but the explorer doesn't
+// work without it even though nothing explicitly depends on it. So I'm adding
+// an explicit dependency here.
+import 'rxjs';
+
 const getLibrary = (provider) => {
   const library = new Web3(provider);
   return library;
